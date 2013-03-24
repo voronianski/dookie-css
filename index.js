832d0bb98a33bf0d763a6f7e1382ce6c960deb4a
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-03-24 21:36:40 +0200

    dookie css lib - start dev, settings, reset, helpers stylus

---
+++ index.js
@@ -0,0 +1,15 @@
+var stylus = require('stylus');
+
+function middleware (str, path) {
+	return stylus(str).set('filename', path).use(css());
+}
+
+function css () {
+	return function (style) {
+		style.include(__dirname);
+		style.import('lib');
+	}
+}
+
+exports.middleware = middleware;
+exports.css = css;
